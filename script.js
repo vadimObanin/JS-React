791e80d2e890e8fc2957aa1fe5ac85dfd34eb3cc
Vadim Obanin <mortido3@mail.ru>
2020-09-03 16:23:15 +0300

    Task number 15. Solved with var. Using the lexical scope.

--- script.js
+++ script.js
@@ -8,8 +8,23 @@ const personalMovieDB = {
   private: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов", "");
-const b = prompt("На сколько оцените его", "");
+for (let i = 0; i < 2; i++) {
+  do {
+    var a = prompt("Один из последних просмотренных фильмов", "");
+  } while (a === null || a.length > 50 || a === "");
+  do {
+    var b = prompt("На сколько оцените его", "");
+  } while (b === null || b.length > 50 || b === "");
 
-personalMovieDB.movies[a] = b;
-console.log(personalMovieDB.movies);
+  personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count <= 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 31) {
+  alert("вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
