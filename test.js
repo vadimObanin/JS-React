ee8f664d257d6f794ecbe7ea33646b92b5756d38
Vadim Obanin <mortido3@mail.ru>
2020-09-10 20:36:48 +0300

    prototype

--- test.js
+++ test.js
@@ -9,3 +9,26 @@ const lena = {
 const lera = Object.create(lena);
 const igor = lera.foo();
 console.log(igor);
+
+const a = 3;
+const b = a;
+
+function copy(mainObj) {
+  const objCopy = {};
+
+  let key;
+  for (key in mainObj) {
+    objCopy[key] = mainObj[key];
+  }
+  return objCopy;
+}
+
+const numbers = {
+  a: 4,
+  b: 3,
+  c: {
+    v: 9,
+    m: 5,
+  },
+};
+const newNumbers = copy(numbers);
