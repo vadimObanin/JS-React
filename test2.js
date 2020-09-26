f2fa5c9526e87503ea3393668437bce8eb02ed7b
Vadim Obanin <mortido3@mail.ru>
2020-09-26 18:48:54 +0300

    031

--- test2.js
+++ test2.js
@@ -1,2 +1,17 @@
-div.insertAdjacentHTML("beforebegin", '<h1>Hello world</h1>);
-    
+const but = document.querySelector("button");
+const deleteElement = (e) => {
+  console.log(e.target);
+};
+
+but.addEventListener("click", deleteElement);
+but.removeEventListener("click", deleteElement);
+
+const link = document.querySelector("a"); //получаем любой элемент, в данном случае ссылку
+link.addEventListener("click", (e) => {
+  e.preventDefault(); //отменяем стандартное поведение браузера. Обязательно первой строкой
+  console.log("Privet"); // здесь мы пишем те действия которые должны быть выполнены при событии click на элемент link
+});
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", deleteElement, { once: true });
+});
