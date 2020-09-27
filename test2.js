a4e26e490583b6d8c11dae0a728a0a1e38434144
Vadim Obanin <mortido3@mail.ru>
2020-09-27 07:28:47 +0300

    031-2

--- test2.js
+++ test2.js
@@ -5,13 +5,3 @@ const deleteElement = (e) => {
 
 but.addEventListener("click", deleteElement);
 but.removeEventListener("click", deleteElement);
-
-const link = document.querySelector("a"); //получаем любой элемент, в данном случае ссылку
-link.addEventListener("click", (e) => {
-  e.preventDefault(); //отменяем стандартное поведение браузера. Обязательно первой строкой
-  console.log("Privet"); // здесь мы пишем те действия которые должны быть выполнены при событии click на элемент link
-});
-
-btns.forEach((btn) => {
-  btn.addEventListener("click", deleteElement, { once: true });
-});
